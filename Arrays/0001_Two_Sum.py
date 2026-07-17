a2dc2161f61e6ffbfbfb5badb26142e1a89f4f6e
honey7236 <shrikantjain722@gmail.com>
2026-07-17 18:12:43 +0530

    remove file structure

--- Arrays/0001_Two_Sum.py
+++
@@ -1,9 +0,0 @@
-# LeetCode 1: Two Sum
-# https://leetcode.com/problems/two-sum/
-# Difficulty: Easy
-
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        # Time Complexity: O(N)
-        # Space Complexity: O(N)
-        pass
--- Strings/0125_Valid_Palindrome.py
+++
@@ -1,9 +0,0 @@
-# LeetCode 125: Valid Palindrome
-# https://leetcode.com/problems/valid-palindrome/
-# Difficulty: Easy
-
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        # Time Complexity: O(N)
-        # Space Complexity: O(1)
-        pass
